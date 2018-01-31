15afad7182ee6d10b7d509334f06bedcea452f2a
sandhiyakarthicsaran <35221190+sandhiyakarthicsaran@users.noreply.github.com>
2018-01-31 16:42:40 +0530

    alpha
    alpha

---
+++ ALPHA.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("Enter the Letter:\n",ch);
+scanf("%c",&ch);
+if((ch>='a'&&ch<='z')//(ch>='A'&&ch<='Z'))
+{
+printf("%c is an alphabet\n",ch);
+}
+else
+{
+printf("%c is not a alphabet\n",ch);
+}
+return 0;
+}
